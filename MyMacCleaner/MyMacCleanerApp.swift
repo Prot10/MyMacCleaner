00431dc6b905a5868d06bc17a0d81225930e812e
Andrea Protani <andrea.protani@outlook.it>
2025-12-09 12:47:17 +0100

    Initial Commit

---
+++ MyMacCleaner/MyMacCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMacCleanerApp.swift
+//  MyMacCleaner
+//
+//  Created by Andrea Protani on 09/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyMacCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyMacCleanerTests/MyMacCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyMacCleanerTests.swift
+//  MyMacCleanerTests
+//
+//  Created by Andrea Protani on 09/12/25.
+//
+
+import Testing
+@testable import MyMacCleaner
+
+struct MyMacCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
